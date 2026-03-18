a1dedf0a74c8b6281b25656a0c1b388443931f49
portbound <token.api.guy@gmail.com>
2026-03-18 16:53:32 -0400

    add server

---
+++ cmd/server/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	"github.com/portbound/busser/internal/server"
+	pb "github.com/portbound/busser/proto"
+	"google.golang.org/grpc"
+)
+
+const port string = ":8080"
+
+func main() {
+	l, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("listen on %s: %v", port, err)
+	}
+
+	s := grpc.NewServer()
+	pb.RegisterMsgBusServiceServer(s, &server.Server{})
+	log.Printf("server running on %s\n", port)
+
+	if err := s.Serve(l); err != nil {
+		log.Fatalf("serve: %v", err)
+	}
+}
