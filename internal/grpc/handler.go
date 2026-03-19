baa6e992a59b33a608862ab031b35539677369ed
portbound <token.api.guy@gmail.com>
2026-03-19 15:28:02 -0400

    add stubbing out gRPC Subscribe handler

--- internal/grpc/handler.go
+++ internal/grpc/handler.go
@@ -18,6 +18,11 @@ func New(b *bus.Bus) *Handler {
 }
 
 func (h *Handler) Publish(ctx context.Context, req *pb.PublishRequest) (*pb.PublishResponse, error) {
+	err := h.bus.Publish(ctx, req.Topic, req.Payload)
+	if err != nil {
+		return nil, err
+	}
+
 	return nil, nil
 }
 
@@ -29,6 +34,22 @@ func (h *Handler) ListTopics(context.Context, *pb.ListTopicsRequest) (*pb.ListTo
 	return nil, nil
 }
 
-func (h *Handler) Subscribe(*pb.SubscribeRequest, grpc.ServerStreamingServer[pb.SubscribeResponse]) error {
+func (h *Handler) Subscribe(req *pb.SubscribeRequest, stream grpc.ServerStreamingServer[pb.SubscribeResponse]) error {
+	ctx := context.Background()
+	ch, err := h.bus.Subscribe(ctx, req.GroupId, req.Topic)
+	if err != nil {
+		return err
+	}
+
+	for e := range ch {
+		stream.Send(&pb.SubscribeResponse{
+			MessageId: e.Id,
+			Topic:     e.Topic,
+			Payload:   e.Payload,
+			Timestamp: e.Timestamp.Unix(), // TODO: should the *Event just be storing this as unix timestamp instead? Or is this conversion fine
+		})
+	}
+
+	// TODO: what kind of error are we returning?
 	return nil
 }
